bdae6656b395876bd7746db5790f01282b8a30ec
Ezra Verheijen <everheijen@trimm.nl>
2016-11-10 20:21:44 +0100

    added some improvements

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# If Modified Since
+# If-Modified-Since
 
-PHP class to handle the `If Modifed Since` functionality as described [here](https://www.feedthebot.com/ifmodified.html).
+![Version](https://img.shields.io/badge/version-1.1.0-green.svg) ![License](https://img.shields.io/badge/license-MIT-green.svg) ![PHP Version](https://img.shields.io/badge/PHP-5.4%2B-red.svg)
 
-**NOTE**: This code always needs to run before ANY other output!
+*Version 1.1.0*
+
+PHP class to handle the `If-Modifed-Since` functionality as described [here](https://varvy.com/ifmodified.html).
+
+## Setup
+
+**IMPORTANT**: This code always needs to run before ANY other output!
 
 ### Example 1:
 
@@ -32,5 +38,30 @@ add_action( 'template_redirect', function() {
 } );
 ```
 
+## Changelog
 
-That's it!
+**1.1.0**
+
+- Removed support for PHP < 5.4
+
+**1.0.0**
+
+- Initial release
+
+## Requirements
+
+- PHP 5.4+
+
+## Disclaimer
+
+This software is provided "as is" with no guarantee. Use it at your own risk and always test it yourself before using it in a production environment. If you find any issues, please [create a new issue](https://github.com/ezraverheijen/if-modified-since/issues/new).
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+It is discouraged to use this software in any project that promotes racism, sexism, homophobia, animal abuse, violence or any other form of hate speech.
+
+## Credits
+
+- [Ezra Verheijen](https://github.com/ezraverheijen)
--- if-modified-since.php
+++ if-modified-since.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * If Modified Since
+ * If-Modified-Since
  * 
- * PHP class to handle the `If Modifed Since` functionality.
+ * PHP class to handle the `If-Modifed-Since` functionality.
  * 
- * @link https://www.feedthebot.com/ifmodified.html
+ * @link https://varvy.com/ifmodified.html
  * 
  * @author    Ezra Verheijen <ezra.verheijen@gmail.com>
  * @link      https://github.com/ezraverheijen/if-modified-since
@@ -15,7 +15,7 @@
 class IfModifiedSince
 {
     /**
-     * Constructor.
+     * Constructor
      * 
      * Tests if there's a timestamp to work with and if it's a valid Unix timestamp
      * before setting the headers.
@@ -24,31 +24,24 @@ class IfModifiedSince
      */
     public function __construct($mtime)
     {
-        if (empty($mtime) || !$this->isValidUnixTimestamp($mtime)) {
+        if (empty($mtime) || !$this->isValidUnixTimeStamp($mtime)) {
             throw new Exception('Missing or invalid timestamp');
         }
         
-        $this->setHeaders((int) $mtime);
+        $this->respond((int) $mtime);
     }
     
     /**
-     * Output the `Modified` headers
-     * 
-     * @uses http_response_code() if available (PHP >= 5.4)
+     * Output the `Modified` response
      * 
      * @param int $mtime Unix timestamp the resource was last modified
      */
-    public function setHeaders($mtime)
+    protected function respond($mtime)
     {
         if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) &&
             strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $mtime)
         {
-            if (function_exists('http_response_code')) {
-                http_response_code(304);
-            } else {
-                header($this->getHttpProtocol() . ' 304 Not Modified');
-            }
-            
+            http_response_code(304);         
             exit;
         } else {
             header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $mtime) . ' GMT');
@@ -61,14 +54,9 @@ class IfModifiedSince
      * @param  mixed   $timestamp Value to check
      * @return boolean 
      */
-    public function isValidUnixTimestamp($timestamp)
+    protected function isValidUnixTimeStamp($timestamp)
     {
         return (ctype_digit($timestamp) &&
                 strtotime(date('d-m-Y H:i:s', $timestamp)) === (int) $timestamp);
-    }
-    
-    public function getHttpProtocol()
-    {
-        return (!empty($_SERVER['SERVER_PROTOCOL'])) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1';
     }
 }
